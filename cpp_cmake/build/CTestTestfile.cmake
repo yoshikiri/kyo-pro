e1eb311fae64e447954560bf744062db8575f210
Momori <elise.nicopy@gmail.com>
2020-06-25 00:17:03 +0900

    Clean up repository

--- cpp_cmake/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/momori/work/kyo-pro/cpp_cmake
-# Build directory: /home/momori/work/kyo-pro/cpp_cmake/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
