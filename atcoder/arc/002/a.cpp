062baf26110f8cf3c65bb595a7b316a0f41bbbc6
Momori <elise.nicopy@gmail.com>
2020-07-15 21:05:51 +0900

    7/15

--- atcoder/arc/002/a.cpp
+++ atcoder/arc/002/a.cpp
@@ -6,6 +6,17 @@ using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); ++i)
 
 int main() {
-  //
+  int y;
+  cin >> y;
+  bool ok = false;
+  if (y % 400 == 0)
+    ok = true;
+  else if (y % 100 == 0)
+    ok = false;
+  else if (y % 4 == 0)
+    ok = true;
+  else
+    ok = false;
+  cout << (ok ? "YES" : "NO") << endl;
   return 0;
 }
--- atcoder/arc/002/b.cpp
+++ atcoder/arc/002/b.cpp
@@ -5,7 +5,55 @@ using ll = long long;
 
 #define rep(i, n) for (int i = 0; i < (int)(n); ++i)
 
+bool check(int y) {
+  bool ok = false;
+  if (y % 400 == 0)
+    ok = true;
+  else if (y % 100 == 0)
+    ok = false;
+  else if (y % 4 == 0)
+    ok = true;
+  else
+    ok = false;
+  return ok;
+}
+
 int main() {
-  //
+  string s;
+  cin >> s;
+  int Y = atoi(s.substr(0, 4).c_str());
+  int M = atoi(s.substr(5, 2).c_str());
+  int D = atoi(s.substr(8, 2).c_str());
+  // cout << Y << m << d;
+  set<int> sm;
+  sm.insert(1);
+  sm.insert(3);
+  sm.insert(5);
+  sm.insert(7);
+  sm.insert(8);
+  sm.insert(10);
+  sm.insert(12);
+
+  for (int y = Y; y < 10000; ++y) {
+    int fm = (y == Y ? M : 1);
+    for (int m = fm; m <= 12; ++m) {
+      int fd = (m == M ? D : 1);
+      int ld = 30;
+      if (sm.find(m) != sm.end())
+        ld = 31;
+      else if (m == 2) {
+        if (check(y))
+          ld = 29;
+        else
+          ld = 28;
+      }
+      for (int d = fd; d <= ld; ++d) {
+        if (y % (m * d) == 0) {
+          printf("%4d/%02d/%02d\n", y, m, d);
+          return 0;
+        }
+      }
+    }
+  }
   return 0;
 }
