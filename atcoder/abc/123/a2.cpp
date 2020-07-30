65d0163727afd3ef612dcf3e75d8e1cc2a26b208
Momori <elise.nicopy@gmail.com>
2020-07-30 22:29:04 +0900

    7/30

--- atcoder/abc/123/a2.cpp
+++ atcoder/abc/123/a2.cpp
@@ -6,6 +6,11 @@ using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); ++i)
 
 int main() {
-  //
+  int n = 5;
+  int a[n];
+  rep(i, n) cin >> a[i];
+  int k;
+  cin >> k;
+  cout << (abs(a[0] - a[4]) <= k ? "Yay!" : ":(") << endl;
   return 0;
 }
---
+++ atcoder/abc/123/c2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+
+int main() {
+  ll n;
+  cin >> n;
+  ll a[5];
+  rep(i, 5) cin >> a[i];
+
+  ll mn = 1e15 + 1;
+  rep(i, 5) mn = min(mn, a[i]);
+  ll g = (n + mn - 1) / mn;
+  cout << 5 + g - 1 << endl;
+  return 0;
+}
